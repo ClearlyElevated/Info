7d0a4958e83dd42f2a8778df597db18c587c9882
ClearlyElevated <51843882+ClearlyElevated@users.noreply.github.com>
2020-08-31 01:25:38 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'ClearlyElevated',
+  tagline: 'Docs & Articles',
+  url: 'https://clearlyelevated.github.io/info',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
